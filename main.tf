7d0e052e2895b08a30e3286326d8985301b2345a
Jaeseong Lee <jaeseongtech@gmail.com>
2025-12-11 08:00:18 +0000

    add prod

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ module "hashicat_infra" {
   source = "./hashicat-infrastructure"
   prefix = "dev"
 }
+
+module "hashicat_prod" {
+  source = "./hashicat-infrastructure"
+  prefix = "prod"
+}
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "website_ip" {
   description = "IP of the application website"
   value       = module.hashicat_infra.catapp_ip
 }
+
+output "prod_website_url" {
+  description = "URL of the application website"
+  value       = module.hashicat_prod.catapp_url
+}
+
+output "prod_website_ip" {
+  description = "IP of the application website"
+  value       = module.hashicat_prod.catapp_ip
+}
